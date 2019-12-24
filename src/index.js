d3db8191f73207c9eb576524c4c6d16b6dcfcb75
jean sidharta <jeansidharta@gmail.com>
2019-12-24 03:34:51 -0300

    installed mongoose

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-console.log('Hello, world!')
+const mongoose = require('mongoose')
+
+mongoose.connect(
+  'mongodb+srv://admin:1234567890@cluster0-xcndn.mongodb.net/test?retryWrites=true&w=majority',
+  { useNewUrlParser: true, useUnifiedTopology: true }
+)
+
+mongoose.connection.on('error', (e) => {
+  console.error('Error connecting to the server!')
+  console.error(e)
+})
+
+mongoose.connection.on('open', () => {
+  console.log('Connected successfuly to the server!')
+})
