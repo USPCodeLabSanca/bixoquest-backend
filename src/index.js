a9a91e27550a1dd37659c55147f7380af7f50f5c
jean sidharta <jeansidharta@gmail.com>
2019-12-24 03:52:01 -0300

    installed express

--- src/index.js
+++ src/index.js
@@ -1,4 +1,9 @@
+const express = require('express')
 const mongoose = require('mongoose')
+const process = require('process')
+
+const app = express()
+const port = process.env.PORT || 3000
 
 mongoose.connect(
   'mongodb+srv://admin:1234567890@cluster0-xcndn.mongodb.net/test?retryWrites=true&w=majority',
@@ -6,10 +11,13 @@ mongoose.connect(
 )
 
 mongoose.connection.on('error', (e) => {
-  console.error('Error connecting to the server!')
+  console.error('Error connecting to MongoDB!')
   console.error(e)
 })
 
 mongoose.connection.on('open', () => {
-  console.log('Connected successfuly to the server!')
+  console.log('Connected successfuly to MongoDB!')
+  app.listen(port, () => {
+    console.log(`Now listening at port ${port} for requests!`)
+  })
 })
