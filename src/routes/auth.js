cd743508ac6942b71c79193b0330af2cd64de4c7
jean sidharta <jeansidharta@gmail.com>
2019-12-28 05:45:58 -0300

    created usp auth mock

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,15 +1,18 @@
 const { Router } = require('express')
 
+const { default: Axios } = require('axios')
+
 const Response = require('../lib/response')
 const { validateRequest, authValidators } = require('../lib/validators')
 const UserModel = require('../models/user')
+const UserUspModel = require('../models/user-usp')
 
 const router = Router()
 
-// Create user
+// Login user
 router.post('/mock-auth-usp', validateRequest(authValidators.mockAuthUsp, async (req, res) => {
   const { nusp, password } = req.body
-  const user = await UserModel.findOne({ nusp })
+  const user = await UserUspModel.findOne({ nusp })
   if (!user) {
     return Response.failure('Invalid credentials', 401).send(res)
   }
@@ -20,8 +23,22 @@ router.post('/mock-auth-usp', validateRequest(authValidators.mockAuthUsp, async 
 }))
 
 // Login user
-router.post('/login', validateRequest(authValidators.login, async (req, res) => {
-  Response.success().send(res)
+router.post('/', validateRequest(authValidators.mockAuthUsp, async (req, res) => {
+  let response
+  try {
+    response = await Axios.post('http://localhost:5000/auth/mock-auth-usp', req.body)
+  } catch (e) {
+    const { response } = e
+    if (!response) Response.failure(undefined, 500).send(res)
+    else return Response.failure(response.data.message).send(res)
+  }
+  const user = response.data.data
+  const dbUser = await UserModel.findOne({ nusp: user.nusp })
+  console.log(dbUser)
+  if (!dbUser) {
+    await UserModel.create(user)
+  }
+  Response.success(user).send(res)
 }))
 
 // get user - user privileges
