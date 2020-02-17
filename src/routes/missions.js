81a14b560e12aed06d8a7d5e13af7e59c28e61c4
Rafael Doering <rafaelsoares@tokenlab.com.br>
2020-02-16 23:56:54 -0300

    remove mission validation

--- src/routes/missions.js
+++ src/routes/missions.js
@@ -1,6 +1,5 @@
 const { Router } = require('express');
 
-const { validateRequestQuery, missionValidators } = require('../lib/validators');
 const { withAuthorization } = require('../lib/jwt');
 
 const MissionsController = require('../controllers/missions');
@@ -11,13 +10,7 @@ router.get('/all', withAuthorization(MissionsController.getAllMissions));
 
 router.get('/:id', withAuthorization(MissionsController.getMission));
 
-router.get(
-  '/',
-  validateRequestQuery(
-    missionValidators.mission,
-    withAuthorization(MissionsController.getNearMissions),
-  ),
-);
+router.get('/', withAuthorization(MissionsController.getNearMissions));
 
 router.post(
   '/:id/complete',
